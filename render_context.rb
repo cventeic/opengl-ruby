d1a441e623eaff34c4c910463234453f87e2c27a
Chris Venteicher <chris@venteicher.org>
2019-01-19 19:14:54 -0600

    render_context: don't store shader ids
    ---

--- render_context.rb
+++ render_context.rb
@@ -1,15 +1,13 @@
 require 'ostruct'
 
 class RenderContext
-  attr_accessor :window, :camera, :camera_last_render, :gl_program_id, :vertex_shader_id, :fragment_shader_id
+  attr_accessor :window, :camera, :camera_last_render, :gl_program_id
 
   def initialize(params = {})
     @window = params.fetch(:window, OpenStruct.new)
     @camera = params.fetch(:camera,
                            Camera.new(aspect_ratio: @window.aspect_ratio))
     @gl_program_id = params.fetch(:gl_program_id, -1)
-    @vertex_shader_id = params.fetch(:vertex_shader_id, -1)
-    @fragment_shader_id = params.fetch(:fragment_shader_id, -1)
     @camera_last_render = nil
   end
 
